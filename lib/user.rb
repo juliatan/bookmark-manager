a008df8f5cde36355826b770c4b1ca7e9ad4e65c
Julia <juliajtan@gmail.com>
2014-06-18 17:23:27 +0100

    User can log in

--- lib/user.rb
+++ lib/user.rb
@@ -38,4 +38,45 @@ class User
     @password = password # needed for test to pass as we created custom writer
     self.password_digest = BCrypt::Password.create(password)
   end
+
+  def self.authenticate(email, password)
+    # that's the user who is trying to sign in
+    user = first(:email => email)
+    # if this user exists and the password provided matches
+    # the one we have password_digest for, everything's fine
+
+    # The Password.new returns an object that overrides the ==
+    # method. Instead of comparing two passwords directly
+    # (which is impossible because we only have a one-way hash)
+    # the == method calculates the candidate password_digest from
+    # the password given and compares it to the password_digest
+    # it was initialised with.
+    # So, to recap: THIS IS NOT A STRING COMPARISON 
+    if user && BCrypt::Password.new(user.password_digest) == password # see below for what code could look like
+      # return this user
+      user
+    else
+      nil
+    end
+
+  end
+end
+
+__END__
+
+module BCrypt
+  class Password
+    def initialize(digest)
+        @digest = digest
+    end
+    def ==(password)        
+        @digest == digest(salt(@digest), password)
+    end
+    def digest(salt, password)
+        # compute the digest
+        # by using bcrypt magic.
+        # return something like
+        # "$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yKUOYTa"
+    end
+  end
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -49,5 +49,34 @@ feature 'User signs up', :focus => true do
     click_button "Sign up"
   end
 
+end
 
+feature "User signs in", :focus => true do
+
+  before(:each) do
+    User.create(:email => "test@test.com",
+                :password => "test",
+                :password_confirmation => "test")
+  end
+
+  scenario "with correct credentials" do
+    visit '/'
+    expect(page).not_to have_content "Welcome, test@test.com"
+    sign_in("test@test.com", "test")
+    expect(page).to have_content "Welcome, test@test.com"
+  end
+
+  scenario "with incorrect credentials" do
+    visit '/'
+    expect(page).not_to have_content "Welcome, test@test.com"
+    sign_in("test@test.com", "wrong")
+    expect(page).not_to have_content "Welcome, test@test.com"
+  end
+
+  def sign_in(email, password)
+    visit '/sessions/new'
+    fill_in 'email', :with => email
+    fill_in 'password', :with => password
+    click_button 'Sign in'
+  end
 end
